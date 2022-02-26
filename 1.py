e0a60dab871179007e5cf17625ac127f75a542f0
Ktos <ktos@cos.pl>
2022-02-26 11:10:56 +0100

    zmienne i działania

--- 1.py
+++ 1.py
@@ -1,4 +1,26 @@
 import sys
+import math
 
 print(sys.version)
-a = 5
+a = 'isi\ngrupa niestacjonarna'
+print(a)
+print(type(a))
+b = 5
+print(b)
+c = 5.4
+print(c)
+d = 3+2j
+print(d)
+print(type(d))
+
+e = 7
+f = 3
+print(e // f)
+print(e ** f)
+
+g = 'wizualizacja danych'
+h = ' grupa 3'
+
+print(g + h)
+
+print(math.pow(e, f))
